0ceecb652d2454a793e69d7ebe77a73ff011eb39
Frizlab <fload@me.com>
2019-02-19 19:22:34 +0100

    Initial Commit

---
+++ ObjCRuntimeFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObjCRuntimeFun
+//
+//  Created by François Lamboley on 2019/2/19.
+//  Copyright © 2019 Frizlab. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
