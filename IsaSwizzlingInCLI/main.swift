febe86a97d3af833393700d1b6933243b9894282
Frizlab <fload@me.com>
2019-03-12 15:36:16 +0100

    Create a new target

---
+++ IsaSwizzlingInCLI/main.swift
@@ -0,0 +1,11 @@
+/*
+ * main.swift
+ * IsaSwizzlingInCLI
+ *
+ * Created by François Lamboley on 2019/3/12.
+ * Copyright © 2019 Frizlab. All rights reserved.
+ */
+
+import Foundation
+
+print("Hello, World!")
